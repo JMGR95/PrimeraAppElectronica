ae4b194c39969e9f0cb744319c9f948a1210677e
JMGR <ingjmiguelgr@gmail.com>
2022-01-25 01:48:10 -0600

    Añadi un segundo comentario

--- app/src/main/java/com/garcia/jose/MainActivity.kt
+++ app/src/main/java/com/garcia/jose/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Este es un comentario
+        //Es es un segundo cometario
     }
 }
